3983ab7d0622251e362158d6f4438fabba5d2292
mocci <tjdwhd3345@gmail.com>
2022-06-23 01:24:53 +0900

    3d 아바타 추가

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import AvatarView from '@sarge/avatar-view';
 
 const Home = () => {
-  return <div>Home</div>;
+  const avtUrl = '/avatar/avatar2.glb';
+  return (
+    <>
+      <div>Home</div>
+      {/* <AvatarView
+        style={{
+          width: '400px',
+          height: '400px',
+          backgroundColor: 'orange',
+          borderRadius: '100%',
+        }}
+        url={avtUrl}
+        rotateAvatar
+        eyeBlink
+        headMovement
+      /> */}
+    </>
+  );
 };
 
 export default Home;
