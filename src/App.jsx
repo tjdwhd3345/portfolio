c20e85dd16e8b08f2a0963655cba4e6d6c3660ae
mocci <tjdwhd3345@gmail.com>
2022-06-23 20:04:21 +0900

    css 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import About from './containers/About';
-import Contact from './containers/Contact';
-import Header from './containers/Header';
-import Home from './containers/Home';
-import Projects from './containers/Projects';
+import About from './containers/About/About';
+import Contact from './containers/Contact/Contact';
+import Footer from './containers/Footer/Footer';
+import Header from './containers/Header/Header';
+import Home from './containers/Home/Home';
+import Projects from './containers/Projects/Projects';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route path='/'>
-            <Route index element={<Home />} />
-            <Route path='about' element={<About />} />
-            <Route path='projects' element={<Projects />} />
-            <Route path='contact' element={<Contact />} />
-          </Route>
-        </Routes>
+        <div className='container'>
+          <Routes>
+            <Route path='/'>
+              <Route index element={<Home />} />
+              <Route path='about' element={<About />} />
+              <Route path='projects' element={<Projects />} />
+              <Route path='contact' element={<Contact />} />
+            </Route>
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </>
   );
